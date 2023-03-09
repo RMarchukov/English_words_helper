1967dcf7659cfe54d977544ed8b3b949740e1a8a
Roman_Marchukov <romamarchukov@icloud.com>
2023-03-09 12:28:54 +0200

    changed db

--- english_words_helper/english_words_helper/settings.py
+++ english_words_helper/english_words_helper/settings.py
@@ -74,14 +74,21 @@ WSGI_APPLICATION = 'english_words_helper.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#             'NAME': 'english',
+#             'USER': 'English',
+#             'PASSWORD': 'English',
+#             'HOST': '127.0.0.1',
+#             'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'english',
-            'USER': 'English',
-            'PASSWORD': 'English',
-            'HOST': '127.0.0.1',
-            'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
